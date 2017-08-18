d5dc076d0983011725582a0fbad9402713325451
saibot777 <Stefan.Trajkovic@ixaris.com>
2017-08-18 09:40:36 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title: string;
+  titleFooter: string;
+  items: Array<any>;
 }
